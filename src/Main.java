60d761cdbf2d4894c8b5b338c1b70c46846a46d6
Aleg2534 <olegkost05@gmail.com>
2022-03-16 22:42:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import hader.HeaderClass1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(228);
+        HeaderClass1 Obj = new HeaderClass1(228, "aboba");
+        System.out.println(Obj.getNum() + "\n" + Obj.getStr() + "\n");
+    }
+}
